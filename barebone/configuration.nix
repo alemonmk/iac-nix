aab0004c6ce88049a6c20588653c5780b0f10851
Lemon Lam <alemonmk@users.noreply.github.com>
2024-10-22 23:36:05 +0800

    Delete sub-subvolumes on boot rootfs wipe

--- barebone/configuration.nix
+++ barebone/configuration.nix
@@ -17,10 +17,21 @@
   boot.initrd.postResumeCommands = ''
     mkdir -p /mnt/btrfs_root
     mount -o subvol=/ /dev/disk/by-partlabel/ROOT /mnt/btrfs_root
+
+    echo "Deleteing sub-subvolumes..."
+    btrfs subvolume list -o /mnt/btrfs_root/rootfs |
+    cut -f9 -d' ' |
+    while read subvolume; do
+      echo "deleting /$subvolume subvolume..."
+      btrfs subvolume delete "/mnt/btrfs_root/$subvolume"
+    done &&
+
     echo "Deleting rootfs subvolume..."
     btrfs subvolume delete /mnt/btrfs_root/rootfs
+
     echo "Restoring blank rootfs subvolume..."
     btrfs subvolume snapshot /mnt/btrfs_root/rootfs-0 /mnt/btrfs_root/rootfs
+
     umount /mnt/btrfs_root
   '';
 
