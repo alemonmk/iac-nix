12857814e21572307c0deefda7c2e4c1aa9f6e31
Lemon Lam <alemonmk@users.noreply.github.com>
2024-10-23 00:03:27 +0800

    Pin flake inputs to avoid suprised installer failure

--- flake.nix
+++ flake.nix
@@ -2,20 +2,20 @@
   description = "";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    impermanence.url = "github:nix-community/impermanence";
-    sops-nix.url = "github:Mic92/sops-nix";
+    nixpkgs.url = "github:nixos/nixpkgs/89172919243df199fe237ba0f776c3e3e3d72367";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/1997e4aa514312c1af7e2bda7fad1644e778ff26";
+    impermanence.url = "github:nix-community/impermanence/e337457502571b23e449bf42153d7faa10c0a562";
+    sops-nix.url = "github:Mic92/sops-nix/26642e8f193f547e72d38cd4c0c4e45b49236d27";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs-unstable";
     sops-nix.inputs.nixpkgs-stable.follows = "nixpkgs";
-    disko.url = "github:nix-community/disko";
+    disko.url = "github:nix-community/disko/4be2aadf13b67ffbb993deb73adff77c46b728fc";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     # unattended-installer.url = "github:chrillefkr/nixos-unattended-installer";
     # unattended-installer.inputs.disko.follows = "disko";
     # unattended-installer.inputs.nixpkgs.follows = "nixpkgs";
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin/64d9d1ae25215c274c37e3e4016977a6779cf0d3";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/5ec753a1fc4454df9285d8b3ec0809234defb975";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
 
