aa294aa05f54c5d40eb509ee35a1805e194c48fb
Lemon Lam <alemonmk@users.noreply.github.com>
2024-10-27 22:22:02 +0800

    feat(nodes/vpngw.nix): Add VPN gateway

---
+++ nodes/vpngw.nix
@@ -0,0 +1,93 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ../base/configuration.nix
+  ];
+
+  networking = {
+    hostName = "rmnmvvpngw";
+    interfaces.ens192.ipv4.addresses = [
+      {
+        address = "10.85.29.3";
+        prefixLength = 28;
+      }
+    ];
+    defaultGateway = {address = "10.85.29.1";};
+    interfaces.ens192.ipv6.addresses = [
+      {
+        address = "2400:8902:e002:59e9::8e01";
+        prefixLength = 64;
+      }
+    ];
+    defaultGateway6 = {address = "2400:8902:e002:59e9::ccef";};
+  };
+
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = "1";
+    "net.ipv6.conf.all.forwarding" = "1";
+  };
+
+  services = {
+    zerotierone = {
+      enable = true;
+      localConf = {
+        physical."2400:8902:e002:5900::/56".blacklist = true;
+        settings.softwareUpdate = "disable";
+      };
+      joinNetworks = ["1fdfc25cb4361829"];
+    };
+    bird2 = {
+      enable = true;
+      config = ''
+      log syslog all;
+      timeformat route iso long;
+      timeformat protocol iso long;
+
+      router id 10.85.29.3;
+      ipv4 table master4;
+      ipv6 table master6;
+
+      protocol device {}
+      protocol direct {
+          interface "ztinv7ire5";
+          ipv4;
+          ipv6;
+      }
+      protocol kernel {
+          ipv4;
+      }
+      protocol kernel {
+          ipv6;
+      }
+      protocol bgp upstream_v4 {
+          local 10.85.29.3 as 65407;
+          neighbor 10.85.29.1 as 65405;
+          keepalive time 20;
+          hold time 60;
+          graceful restart on;
+          long lived graceful restart on;
+          ipv4 {
+              import all;
+              export where net ~ 10.80.105.0/24;
+          };
+      }
+      protocol bgp upstream_v6 {
+          local 2400:8902:e002:59e9::8e01 as 65407;
+          neighbor 2400:8902:e002:59e9::1 as 65405;
+          keepalive time 20;
+          hold time 60;
+          graceful restart on;
+          long lived graceful restart on;
+          ipv4 {
+              import all;
+              export where net ~ 2400:8902:e002:59af::/24;
+          };
+      }
+      '';
+    };
+  };
+}
