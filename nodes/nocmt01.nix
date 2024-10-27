829841c41424d094127d1f3e5fb35bbf9d84e313
Lemon Lam <alemonmk@users.noreply.github.com>
2024-10-27 23:12:23 +0800

    feat(nodes/nocmt01.nix): implement sops-nix secret management

--- nodes/nocmt01.nix
+++ nodes/nocmt01.nix
@@ -8,6 +8,38 @@
     ../base/configuration.nix
   ];
 
+  sops = {
+    age.sshKeyPaths = ["/etc/sh/ssh_host_ed25519_key"];
+    age.generateKey = true;
+    secrets.entra-client-id = {
+      mode = "0440";
+      owner = config.users.users.grafana.name;
+      group = config.users.users.grafana.group;
+      sopsFile = ../secrets/nocmt01/entraid.yaml;
+    };
+    secrets.entra-client-secret = {
+      mode = "0440";
+      owner = config.users.users.grafana.name;
+      group = config.users.users.grafana.group;
+      sopsFile = ../secrets/nocmt01/entraid.yaml;
+    };
+    secrets.ncm-password = {
+      sopsFile = ../secrets/nocmt01/oxidized.yaml;
+    };
+    secrets.monitoring-creds = {
+      mode = "0440";
+      owner = config.users.users.telegraf.name;
+      group = config.users.users.telegraf.group;
+      sopsFile = ../secrets/nocmt01/monitoring.yaml;
+    };
+    templates."oxidized-cfg" = {
+      file = ../blobs/monitoring/oxidized/config.yml;
+      mode = "0440";
+      owner = config.users.users.oxidized.name;
+      group = config.users.users.oxidized.group;
+    };
+  };
+
   networking = {
     hostName = "rmnmvnocmt01";
     interfaces.ens192.ipv4.addresses = [
@@ -17,6 +49,12 @@
       }
     ];
     defaultGateway = {address = "10.85.10.30";};
+    interfaces.ens224.ipv4.addresses = [
+      {
+        address = "10.88.0.2";
+        prefixLength = 24;
+      }
+    ];
     enableIPv6 = false;
   };
 
@@ -32,6 +70,7 @@
       retentionPeriod = 24;
       extraOptions = [
         "-selfScrapeInterval=15s"
+        "-downsampling.period=90d:5m,365d:1h"
         "-promscrape.config.strictParse=false"
         "-promscrape.config=${../blobs/monitoring/victoriametrics/scrape.yml}"
       ];
@@ -60,8 +99,8 @@
         "auth.azuread" = {
           enabled = true;
           allow_sign_up = true;
-          # client_id = config.sops.secrets.grafana.azuread-id;
-          # client_secret = config.sops.secrets.grafana.azuread-secret;
+          client_id = "$__file{${config.sops.secrets.entra-client-id.path}}";
+          client_secret = "$__file{${config.sops.secrets.entra-client-secret.path}}";
           scopes = "openid email profile";
           auth_url = "https://login.microsoftonline.com/da34cce3-0860-45ae-bc8d-37725f6ecdfa/oauth2/v2.0/authorize";
           token_url = "https://login.microsoftonline.com/da34cce3-0860-45ae-bc8d-37725f6ecdfa/oauth2/v2.0/token";
@@ -72,7 +111,7 @@
 
     oxidized = {
       enable = true;
-      # configFile = config.sops.secrets.oxidized.cfg.path;
+      configFile = config.sops.templates.oxidized-cfg.path;
       routerDB = ../blobs/monitoring/oxidized/routers.db;
     };
 
