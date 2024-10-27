29fda0ef2c854c5ee5f4dd231e39c4e33cbe8a23
Lemon Lam <alemonmk@users.noreply.github.com>
2024-10-27 23:16:10 +0800

    nix fmt

--- nodes/vpngw.nix
+++ nodes/vpngw.nix
@@ -43,50 +43,50 @@
     bird2 = {
       enable = true;
       config = ''
-      log syslog all;
-      timeformat route iso long;
-      timeformat protocol iso long;
+        log syslog all;
+        timeformat route iso long;
+        timeformat protocol iso long;
 
-      router id 10.85.29.3;
-      ipv4 table master4;
-      ipv6 table master6;
+        router id 10.85.29.3;
+        ipv4 table master4;
+        ipv6 table master6;
 
-      protocol device {}
-      protocol direct {
-          interface "ztinv7ire5";
-          ipv4;
-          ipv6;
-      }
-      protocol kernel {
-          ipv4;
-      }
-      protocol kernel {
-          ipv6;
-      }
-      protocol bgp upstream_v4 {
-          local 10.85.29.3 as 65407;
-          neighbor 10.85.29.1 as 65405;
-          keepalive time 20;
-          hold time 60;
-          graceful restart on;
-          long lived graceful restart on;
-          ipv4 {
-              import all;
-              export where net ~ 10.80.105.0/24;
-          };
-      }
-      protocol bgp upstream_v6 {
-          local 2400:8902:e002:59e9::8e01 as 65407;
-          neighbor 2400:8902:e002:59e9::1 as 65405;
-          keepalive time 20;
-          hold time 60;
-          graceful restart on;
-          long lived graceful restart on;
-          ipv4 {
-              import all;
-              export where net ~ 2400:8902:e002:59af::/24;
-          };
-      }
+        protocol device {}
+        protocol direct {
+            interface "ztinv7ire5";
+            ipv4;
+            ipv6;
+        }
+        protocol kernel {
+            ipv4;
+        }
+        protocol kernel {
+            ipv6;
+        }
+        protocol bgp upstream_v4 {
+            local 10.85.29.3 as 65407;
+            neighbor 10.85.29.1 as 65405;
+            keepalive time 20;
+            hold time 60;
+            graceful restart on;
+            long lived graceful restart on;
+            ipv4 {
+                import all;
+                export where net ~ 10.80.105.0/24;
+            };
+        }
+        protocol bgp upstream_v6 {
+            local 2400:8902:e002:59e9::8e01 as 65407;
+            neighbor 2400:8902:e002:59e9::1 as 65405;
+            keepalive time 20;
+            hold time 60;
+            graceful restart on;
+            long lived graceful restart on;
+            ipv4 {
+                import all;
+                export where net ~ 2400:8902:e002:59af::/64;
+            };
+        }
       '';
     };
   };
