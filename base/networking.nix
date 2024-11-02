3a6825bc2347d83983cefa278d7ec30b2e6ebd17
Lemon Lam <alemonmk@users.noreply.github.com>
2024-11-02 14:13:20 +0800

    feat(base/networking.nix): change DNS servers

--- base/networking.nix
+++ base/networking.nix
@@ -3,7 +3,7 @@
     domain = "snct.rmntn.net";
     useDHCP = false;
     timeServers = ["rmnmvntpsrv01.snct.rmntn.net"];
-    nameservers = ["10.85.11.1" "10.85.11.2"];
+    nameservers = ["10.85.10.1" "10.85.10.2" "2400:8902:e002:59e0::d:1" "2400:8902:e002:59e0::d:2"];
     firewall.enable = false;
   };
   boot.kernel.sysctl = {
