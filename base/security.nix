b284f1ba713623212e462f79e89a7c9ffb6466bf
Lemon Lam <alemonmk@users.noreply.github.com>
2024-12-03 09:32:24 +0800

    fix(base/security.nix): stop generating RSA host keys

--- base/security.nix
+++ base/security.nix
@@ -62,6 +62,12 @@
         AllowAgentForwarding no
         AllowStreamLocalForwarding no
       '';
+      hostKeys = [
+        {
+          type = "ed25519";
+          path = "/etc/ssh/ssh_host_ed25519_key";
+        }
+      ];
     };
   };
 }
