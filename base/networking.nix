5e3769849a58b784bff1a6164dc83c202e66e541
Lemon Lam <alemonmk@users.noreply.github.com>
2024-12-15 21:22:41 +0800

    fix(base/networking.nix): drop bbr
    https://github.com/nix-community/srvos/pull/556

--- base/networking.nix
+++ base/networking.nix
@@ -6,8 +6,4 @@
     nameservers = ["10.85.10.1" "10.85.10.2" "2400:8902:e002:59e0::d:1" "2400:8902:e002:59e0::d:2"];
     firewall.enable = false;
   };
-  boot.kernel.sysctl = {
-    "net.core.default_qdisc" = "fq";
-    "net.ipv4.tcp_congestion_control" = "bbr";
-  };
 }
