faafe93ea8283116dfa6bde0afc013bd7d0617f7
Lemon Lam <alemonmk@users.noreply.github.com>
2025-01-28 21:30:22 +0800

    feat(nix/overlays/next.nix): override package for 13.4 and separate proxy patch

--- overlays/next.nix
+++ overlays/next.nix
@@ -1,7 +1,32 @@
 final: prev: {
   squid = prev.squid.overrideAttrs (old: {
-    configureFlags = 
+    configureFlags =
       (prev.lib.lists.remove "--enable-ipv6" old.configureFlags)
       ++ ["--disable-ipv6" "--disable-esi"];
+  });
+
+  technitium-dns-server-library = prev.technitium-dns-server-library.overrideAttrs (old: rec {
+    version = "dns-server-v13.4";
+    src = prev.fetchFromGitHub {
+      owner = "TechnitiumSoftware";
+      repo = "TechnitiumLibrary";
+      tag = version;
+      hash = "sha256-mNWEQAN7T29GmX3mGPrJ5GhELUYNJTvUAhyEWZwN5Vw=";
+      name = "${old.pname}-${version}";
+    };
+  });
+
+  technitium-dns-server = prev.technitium-dns-server.overrideAttrs (old: rec {
+    version = "13.4";
+    src = prev.fetchFromGitHub {
+      owner = "TechnitiumSoftware";
+      repo = "DnsServer";
+      tag = "v${version}";
+      hash = "sha256-ikq+0n/VqSxG2Hnn802EkHFpmsn25expBQnTJ69gktk=";
+      name = "${old.pname}-${version}";
+    };
+    patches = [
+      ../blobs/technitium-dns-server/webReqProxy.patch
+    ];
   });
 }
