64488c0d8cd5a734bac80cb9363f5bc43906aefe
Lemon Lam <alemonmk@users.noreply.github.com>
2025-03-08 00:20:36 +0800

    update(overlays/next.nix): Technitium/DnsServer 13.4 -> 13.4.3

--- overlays/next.nix
+++ overlays/next.nix
@@ -4,23 +4,23 @@ final: prev: {
   };
 
   technitium-dns-server-library = prev.technitium-dns-server-library.overrideAttrs (old: rec {
-    version = "dns-server-v13.4";
+    version = "dns-server-v13.4.2";
     src = prev.fetchFromGitHub {
       owner = "TechnitiumSoftware";
       repo = "TechnitiumLibrary";
       tag = version;
-      hash = "sha256-mNWEQAN7T29GmX3mGPrJ5GhELUYNJTvUAhyEWZwN5Vw=";
+      hash = "sha256-46RoUxfMMVqh5DQjY3Q8JpIE1afljduHvbBgL1n7suA=";
       name = "${old.pname}-${version}";
     };
   });
 
   technitium-dns-server = prev.technitium-dns-server.overrideAttrs (old: rec {
-    version = "13.4";
+    version = "13.4.3";
     src = prev.fetchFromGitHub {
       owner = "TechnitiumSoftware";
       repo = "DnsServer";
       tag = "v${version}";
-      hash = "sha256-ikq+0n/VqSxG2Hnn802EkHFpmsn25expBQnTJ69gktk=";
+      hash = "sha256-4RPJS/mxGCre6BYI75z71QPxx+ExOKy0r/Vr2dxLPWY=";
       name = "${old.pname}-${version}";
     };
     patches = [
