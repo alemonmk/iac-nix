5c349f4f3c2cda87c2af2aca8d6edb0f96fd3a4f
Lemon Lam <alemonmk@users.noreply.github.com>
2025-03-07 09:58:50 +0800

    update(pkgs/prometheus-chrony-exporter): 0.11.0 -> 0.12.0

--- pkgs/prometheus-chrony-exporter.nix
+++ pkgs/prometheus-chrony-exporter.nix
@@ -5,22 +5,22 @@
 }:
 buildGoModule rec {
   pname = "chrony-exporter";
-  version = "0.11.0";
+  version = "0.12.0";
 
   src = fetchFromGitHub {
     owner = "SuperQ";
     repo = "chrony_exporter";
-    rev = "5079704a720fdc8d592b6c4e97de0ced2b980b2b";
-    sha256 = "sha256-xHI0BYli82cCGFtv58jJ+PpjR/q1ReWjUqRDtp89w+Y=";
+    rev = "f95d00763f6291742e51326402102ffd68d6d5cf";
+    sha256 = "sha256-ZXqCZZx0UG8050SYgVwD+wnTX0N41Bjv1dhdQmOHmR4=";
   };
 
-  vendorHash = "sha256-czAkqEeGRSdMJmSZNwzAMAk045p4aog4XOQUU48jeOo=";
+  vendorHash = "sha256-3zL7BrCdMVnt7F1FiZ2eQnKVhmCeW3aYKKX9v01ms/k=";
 
-  meta = with lib; {
+  meta = {
     description = "Prometheus Exporter for Chrony NTP.";
     mainProgram = "chrony_exporter";
     homepage = "https://github.com/SuperQ/chrony_exporter";
-    license = licenses.asl20;
-    maintainers = [];
+    license = lib.licenses.asl20;
+    maintainers = with lib; [];
   };
 }
