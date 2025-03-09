129581da1475a6c49cfac6c55f68fae4d7ea0853
Lemon Lam <alemonmk@users.noreply.github.com>
2025-03-09 22:49:29 +0800

    feat(lib/default.nix): full iPXE stage1 installer in one flake output

--- lib/default.nix
+++ lib/default.nix
@@ -8,55 +8,67 @@
   disko,
   ...
 } @ inputs: {
-  stage1Installer = nixpkgs.lib.nixosSystem {
-    inherit system;
-    specialArgs = {inherit inputs;};
-    modules = [
-      self.stage1InstallerModules
-      ({
-        modulesPath,
-        lib,
-        pkgs,
-        ...
-      }: {
-        imports = [
-          "${modulesPath}/installer/netboot/netboot-minimal.nix"
-          "${modulesPath}/profiles/perlless.nix"
-          ./stage1installer/system.nix
-          ./stage1installer/minimalize.nix
-        ];
-        disabledModules = ["profiles/base.nix"];
-        unattendedInstaller = {
-          enable = true;
-          target = nixpkgs.lib.nixosSystem {
-            modules = [
-              disko.nixosModules.disko
-              ../barebone/diskolayout.nix
-              ({...}: {
-                nixpkgs.hostPlatform = "x86_64-linux";
-                system.stateVersion = "24.11";
-              })
-            ];
+  stage1Installer = let
+    image = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {inherit inputs;};
+      modules = [
+        self.stage1InstallerModules
+        ({
+          modulesPath,
+          lib,
+          pkgs,
+          ...
+        }: {
+          imports = [
+            "${modulesPath}/installer/netboot/netboot-minimal.nix"
+            "${modulesPath}/profiles/perlless.nix"
+            ./stage1installer/system.nix
+            ./stage1installer/minimalize.nix
+          ];
+          disabledModules = ["profiles/base.nix"];
+          unattendedInstaller = {
+            enable = true;
+            target = nixpkgs.lib.nixosSystem {
+              modules = [
+                disko.nixosModules.disko
+                ../barebone/diskolayout.nix
+                ({...}: {
+                  nixpkgs.hostPlatform = "x86_64-linux";
+                  system.stateVersion = "24.11";
+                })
+              ];
+            };
+            flake = "git+https://code.rmntn.net/iac/nix?ref=main#barebone";
+            postDisko = ''
+              umount -Rv /mnt
+              mount -o subvol=/ /dev/disk/by-partlabel/ROOT /mnt
+              btrfs subvolume snapshot -r /mnt/rootfs /mnt/rootfs-0
+              umount -v /mnt
+              mount -o noatime,subvol=/rootfs /dev/disk/by-partlabel/ROOT /mnt
+              mount -o umask=0077 -t vfat /dev/disk/by-partlabel/EFI /mnt/boot
+              mount -t ext4 /dev/disk/by-partlabel/NIX /mnt/nix
+            '';
+            preInstall = ''
+              mkdir -p /mnt/nix/persist/{etc/{nixos,ssh},var/{lib/nixos,lib/sss,log}}
+              mount -o bind -m /mnt/nix/persist/etc/nixos /mnt/etc/nixos
+              mount -o bind -m /mnt/nix/persist/var/log /mnt/var/log
+            '';
           };
-          flake = "git+https://code.rmntn.net/iac/nix?ref=main#barebone";
-          postDisko = ''
-            umount -Rv /mnt
-            mount -o subvol=/ /dev/disk/by-partlabel/ROOT /mnt
-            btrfs subvolume snapshot -r /mnt/rootfs /mnt/rootfs-0
-            umount -v /mnt
-            mount -o noatime,subvol=/rootfs /dev/disk/by-partlabel/ROOT /mnt
-            mount -o umask=0077 -t vfat /dev/disk/by-partlabel/EFI /mnt/boot
-            mount -t ext4 /dev/disk/by-partlabel/NIX /mnt/nix
-          '';
-          preInstall = ''
-            mkdir -p /mnt/nix/persist/{etc/{nixos,ssh},var/{lib/nixos,lib/sss,log}}
-            mount -o bind -m /mnt/nix/persist/etc/nixos /mnt/etc/nixos
-            mount -o bind -m /mnt/nix/persist/var/log /mnt/var/log
-          '';
-        };
-      })
-    ];
-  };
+        })
+      ];
+    };
+  in
+    with image;
+      pkgs.symlinkJoin {
+        name = "netbootImage";
+        paths = with config.system.build; [
+          netbootRamdisk
+          kernel
+          netbootIpxeScript
+        ];
+        preferLocalBuild = true;
+      };
   stage1System = nixpkgs.lib.nixosSystem {
     modules = [
       disko.nixosModules.disko
