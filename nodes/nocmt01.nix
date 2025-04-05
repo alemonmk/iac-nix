b6e8194439ac975c681b4c575f8247a43da0383a
Lemon Lam <alemonmk@users.noreply.github.com>
2025-04-06 01:07:00 +0800

    update(pkgs/oxidized): override oxidized from nixpkgs, 0.30.1 -> 0.33.0

--- nodes/nocmt01.nix
+++ nodes/nocmt01.nix
@@ -152,7 +152,7 @@
 
   environment.persistence."/nix/persist".directories = [
     "/var/lib/grafana"
-    "/var/lib/oxidized/store"
+    "/var/lib/oxidized/store.git"
     "/var/lib/victoriametrics"
     "/var/lib/victorialogs"
   ];
--- overlays/stable.nix
+++ overlays/stable.nix
@@ -1,4 +1,5 @@
 final: prev: {
   jellyfin-ffmpeg = final.callPackage ../pkgs/jellyfin-ffmpeg.nix {};
   prometheus-chrony-exporter = final.callPackage ../pkgs/prometheus-chrony-exporter.nix {};
+  oxidized = final.callPackage ../pkgs/oxidized/default.nix {};
 }
