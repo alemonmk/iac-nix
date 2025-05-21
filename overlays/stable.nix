6c86aac7eb8f9bdb0d90fe95597fc2a3a3927a9d
Lemon Lam <alemonmk@users.noreply.github.com>
2025-05-21 18:46:06 +0800

    chore(overlays/stable.nix): drop oxidized overlay
    0.33.0 already in nixpkgs

--- overlays/stable.nix
+++ overlays/stable.nix
@@ -1,5 +1,4 @@
 final: prev: {
   jellyfin-ffmpeg = final.callPackage ../pkgs/jellyfin-ffmpeg.nix {};
   prometheus-chrony-exporter = final.callPackage ../pkgs/prometheus-chrony-exporter.nix {};
-  oxidized = final.callPackage ../pkgs/oxidized/default.nix {};
 }
