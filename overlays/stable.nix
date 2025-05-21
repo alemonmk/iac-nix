194233db56fd3682679e32d6ec65ef17d91fa69e
Lemon Lam <alemonmk@users.noreply.github.com>
2025-05-21 19:38:12 +0800

    refactor(overlays/stable.nix): jellyfin-ffmpeg overrides

--- overlays/stable.nix
+++ overlays/stable.nix
@@ -1,4 +1,27 @@
 final: prev: {
-  jellyfin-ffmpeg = final.callPackage ../pkgs/jellyfin-ffmpeg.nix {};
+  jellyfin-ffmpeg = prev.jellyfin-ffmpeg.override {
+    ffmpeg_7-full = prev.ffmpeg_7-headless.override {
+      withUnfree = true;
+      withAlsa = false;
+      withAmf = false;
+      withCelt = false;
+      withFdkAac = true;
+      withFontconfig = false;
+      withFreetype = false;
+      withHarfbuzz = false;
+      withNvcodec = false;
+      withRist = false;
+      withSpeex = false;
+      withSrt = false;
+      withSsh = false;
+      withTheora = false;
+      withVaapi = false;
+      withRuntimeCPUDetection = false;
+      withHtmlDoc = false;
+      withManPages = false;
+      withPodDoc = false;
+      withTxtDoc = false;
+    };
+  };
   prometheus-chrony-exporter = final.callPackage ../pkgs/prometheus-chrony-exporter.nix {};
 }
