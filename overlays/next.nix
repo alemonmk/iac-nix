0312cce53c3cc93866b97fcdac087bfb355947d6
Lemon Lam <alemonmk@users.noreply.github.com>
2025-05-23 03:34:45 +0800

    chore(overlays/stable.nix): move overlays targeting nixpkgs-next back to nixpkgs

--- overlays/next.nix
+++ overlays/next.nix
@@ -1,9 +1,1 @@
-final: prev: {
-  squid = prev.squid.override {ipv6 = false;};
-
-  technitium-dns-server = prev.technitium-dns-server.overrideAttrs (old: rec {
-    patches = [../blobs/technitium-dns-server/webReqProxy.patch];
-  });
-
-  code-server = final.callPackage ../pkgs/code-server.nix {};
-}
+final: prev: {}
