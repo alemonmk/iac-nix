64a04658aa52957a2e316bda831dc0480dcaa360
Lemon Lam <alemonmk@users.noreply.github.com>
2025-06-11 12:38:13 +0800

    refactor(nodes/shitara/node.nix): move network tuning to base config

--- base/linode/configuration.nix
+++ base/linode/configuration.nix
@@ -18,9 +18,25 @@
   networking = {
     domain = "rmntn.net";
     timeServers = ["ats1.e-timing.ne.jp"];
+    useNetworkd = true;
     useDHCP = false;
     usePredictableInterfaceNames = false;
   };
+  services.resolved.llmnr = "false";
+
+  boot.kernel.sysctl = {
+    "net.core.wmem_max" = 134217728;
+    "net.core.rmem_max" = 134217728;
+    "net.ipv4.tcp_rmem" = "4096 87380 134217728";
+    "net.ipv4.tcp_wmem" = "4096 65536 134217728";
+    "net.core.netdev_max_backlog" = 30000;
+    "net.ipv4.tcp_no_metrics_save" = 1;
+    "net.core.default_qdisc" = "fq";
+  };
+
+  systemd.network.wait-online.enable = false;
+  systemd.services.systemd-networkd.stopIfChanged = false;
+  systemd.services.systemd-resolved.stopIfChanged = false;
 
   services.dbus.implementation = "broker";
 
