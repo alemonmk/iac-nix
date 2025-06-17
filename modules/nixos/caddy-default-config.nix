07905092af115242796775967d008fb647c54174
Lemon Lam <alemonmk@users.noreply.github.com>
2025-06-17 18:55:05 +0800

    fix(modules/nixos/caddy-default-config.nix): fix deprecated {arg.0} warning

--- modules/nixos/caddy-default-config.nix
+++ modules/nixos/caddy-default-config.nix
@@ -10,22 +10,21 @@
         email = "apps@snct.rmntn.net";
         globalConfig = ''
           persist_config off
-          admin
           key_type p256
         '';
         extraConfig = ''
           (cors) {
             @cors_preflight method OPTIONS
-            @cors header Origin {args.0}
+            @cors header Origin {args[0]}
             handle @cors_preflight {
-              header Access-Control-Allow-Origin "{args.0}"
+              header Access-Control-Allow-Origin "{args[0]}"
               header Access-Control-Allow-Methods "GET, POST, PUT, PATCH, DELETE"
               header Access-Control-Allow-Headers "Content-Type"
               header Access-Control-Max-Age "3600"
               respond "" 204
             }
             handle @cors {
-              header Access-Control-Allow-Origin "{args.0}"
+              header Access-Control-Allow-Origin "{args[0]}"
               header Access-Control-Expose-Headers "Link"
             }
           }
