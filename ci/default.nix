ce2949496f8d4b39ef1157ddbca6dafda22cfa2e
Lemon Lam <alemonmk@users.noreply.github.com>
2025-06-28 12:44:09 +0800

    chore(nodes/webgw.nix): change to allowInsecurePredicate
    To avoid modifying permittedInsecurePackages everytime squid gets updated

--- ci/default.nix
+++ ci/default.nix
@@ -1,15 +1,16 @@
 inputs: let
   inherit (inputs) self nixpkgs nixpkgs-next;
+  inherit (nixpkgs.lib) elem getName mapAttrs;
   pkgs-stable = import nixpkgs {
     system = "x86_64-linux";
     config.allowUnfree = true;
-    config.permittedInsecurePackages = ["squid-7.0.1"];
+    config.allowInsecurePredicate = pkg: elem (getName pkg) ["squid"];
   };
   pkgs-next = nixpkgs-next.legacyPackages.x86_64-linux;
   stable-overlay-realised = (import ../overlays/stable.nix) pkgs-stable pkgs-stable;
   next-overlay-realised = (import ../overlays/next.nix) pkgs-next pkgs-next;
   overlays-combined = stable-overlay-realised // next-overlay-realised;
 in {
-  hosts.x86_64-linux = nixpkgs.lib.mapAttrs (_: cfg: cfg.config.system.build.toplevel) self.nixosConfigurations;
+  hosts.x86_64-linux = mapAttrs (_: cfg: cfg.config.system.build.toplevel) self.nixosConfigurations;
   pkgs.x86_64-linux = overlays-combined // self.packages.x86_64-linux;
 }
