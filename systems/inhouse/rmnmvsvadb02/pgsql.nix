2b42e9fd37868846619b2cc8bb045dba613f0b5c
Lemon Lam <alemonmk@users.noreply.github.com>
2025-06-28 14:05:07 +0800

    refactor(nodes): nodes -> systems part 2, move adb02 -> svadb02

---
+++ systems/inhouse/rmnmvsvadb02/pgsql.nix
@@ -0,0 +1,27 @@
+{config, ...}: {
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+  };
+
+  virtualisation.oci-containers.containers = {
+    "pgsql-patroni" = {
+      image = "ghcr.io/alemonmk/nomad-pgsql-patroni:17.4-1.tsdb_gis";
+      extraOptions = ["--network=host"];
+      environment.POSTGRES_INITDB_ARGS = "--data-checksums";
+      volumes = [
+        "/nix/persist/var/lib/postgresql:/var/lib/postgresql/data"
+        "${config.sops.secrets.patroni-config.path}:/secrets/patroni.yml"
+      ];
+    };
+  };
+
+  services.etcd.enable = true;
+
+  environment.persistence."/nix/persist" = {
+    directories = [
+      "/var/lib/postgresql"
+      "/var/lib/etcd"
+    ];
+  };
+}
