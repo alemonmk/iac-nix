c9e5c97fd23a3205a2e19a3676b0b1fc489b10c0
Lemon Lam <alemonmk@users.noreply.github.com>
2025-06-28 15:02:35 +0800

    feat(flake.nix): formatter alejandra -> treefmt

---
+++ treefmt.nix
@@ -0,0 +1,19 @@
+{
+  nixpkgs,
+  treefmt-nix,
+  ...
+}: let
+  forEachSystems = f: nixpkgs.lib.genAttrs ["x86_64-linux" "x86_64-darwin"] f;
+  
+  settings = {
+    projectRootFile = "flake.nix";
+    settings.verbose = 1;
+    settings.excludes = ["blobs/*" "secrets/*"];
+
+    programs.terraform.enable = true;
+    programs.nixfmt.enable = true;
+    programs.ruff-format.enable = true;
+  };
+  package = forEachSystems (sys: treefmt-nix.lib.evalModule nixpkgs.legacyPackages.${sys} settings);
+in
+  forEachSystems (sys: package.${sys}.config.build.wrapper)
