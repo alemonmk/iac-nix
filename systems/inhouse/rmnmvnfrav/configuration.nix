45fb30ff0f37dd5363a9e779af240fe74c081a61
Lemon Lam <alemonmk@users.noreply.github.com>
2025-06-28 14:02:51 +0800

    refactor(nodes): nodes -> systems part 2, move vpngw -> nfrav

---
+++ systems/inhouse/rmnmvnfrav/configuration.nix
@@ -0,0 +1,29 @@
+{
+  networking.proxy = {
+    httpProxy = null;
+    httpsProxy = null;
+  };
+
+  systemd.network = {
+    config.networkConfig = {
+      IPv4Forwarding = true;
+      IPv6Forwarding = true;
+    };
+    networks."1-ens192" = {
+      matchConfig.Name = "ens192";
+      address = [
+        "10.85.29.3/28"
+        "2400:8902:e002:59e9::8e01/64"
+      ];
+      gateway = [
+        "10.85.29.1"
+        "2400:8902:e002:59e9::1"
+      ];
+      networkConfig.LLDP = false;
+    };
+  };
+
+  users.ms-ad.enable = false;
+
+  imports = [./remote-access-vpn.nix];
+}
