f6845b457d0624d3b23d84b81b2963b43fd4f61c
Lemon Lam <alemonmk@users.noreply.github.com>
2025-06-30 20:51:51 +0800

    Revert "chore(modules/nixos/caddy-defaults.nix): eliminate excessive brackets"
    This reverts commit c5120467b32ac84de727945986fc31da31004f6f.

--- modules/nixos/caddy-defaults.nix
+++ modules/nixos/caddy-defaults.nix
@@ -4,31 +4,35 @@
   ...
 }:
 {
-  config.services.caddy = lib.mkIf config.services.caddy.enable {
-    acmeCA = "https://atpki.snct.rmntn.net/acme/w1/directory";
-    email = "apps@snct.rmntn.net";
-    globalConfig = ''
-      persist_config off
-      key_type p256
-    '';
-    extraConfig = ''
-      (cors) {
-        @cors_preflight method OPTIONS
-        @cors header Origin {args[0]}
-        handle @cors_preflight {
-          header Access-Control-Allow-Origin "{args[0]}"
-          header Access-Control-Allow-Methods "GET, POST, PUT, PATCH, DELETE"
-          header Access-Control-Allow-Headers "Content-Type"
-          header Access-Control-Max-Age "3600"
-          respond "" 204
-        }
-        handle @cors {
-          header Access-Control-Allow-Origin "{args[0]}"
-          header Access-Control-Expose-Headers "Link"
-        }
-      }
-    '';
-  };
+  config = lib.mkIf config.services.caddy.enable {
+    services = {
+      caddy = {
+        acmeCA = "https://atpki.snct.rmntn.net/acme/w1/directory";
+        email = "apps@snct.rmntn.net";
+        globalConfig = ''
+          persist_config off
+          key_type p256
+        '';
+        extraConfig = ''
+          (cors) {
+            @cors_preflight method OPTIONS
+            @cors header Origin {args[0]}
+            handle @cors_preflight {
+              header Access-Control-Allow-Origin "{args[0]}"
+              header Access-Control-Allow-Methods "GET, POST, PUT, PATCH, DELETE"
+              header Access-Control-Allow-Headers "Content-Type"
+              header Access-Control-Max-Age "3600"
+              respond "" 204
+            }
+            handle @cors {
+              header Access-Control-Allow-Origin "{args[0]}"
+              header Access-Control-Expose-Headers "Link"
+            }
+          }
+        '';
+      };
+    };
 
-  config.environment.persistence."/nix/persist".directories = [ "/var/lib/caddy/.local" ];
+    environment.persistence."/nix/persist".directories = [ "/var/lib/caddy/.local" ];
+  };
 }
