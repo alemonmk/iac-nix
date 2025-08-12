0a0df364e4b6c23493e75155830c1437ff2c6ad9
Lemon Lam <alemonmk@users.noreply.github.com>
2025-08-12 15:30:50 +0800

    feat(home/chisa): add uv tool path to PATH

--- home/chisa/default.nix
+++ home/chisa/default.nix
@@ -8,6 +8,7 @@
     CONSUL_HTTP_ADDR = "http://10.85.183.1:8500";
     SOPS_AGE_KEY_FILE = "$HOME/.sops/key.txt";
   };
+  home.sessionPath = ["$HOME/.local/bin"];
 
   programs.zsh.enable = true;
 
