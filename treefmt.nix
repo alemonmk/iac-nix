6e184be6212d0f695301adf5cfda7ef5fe5b5687
Lemon Lam <alemonmk@users.noreply.github.com>
2025-08-16 16:55:18 +0800

    feat(treefmt.nix): enable jsonfmt, toml-sort and yamlfmt

--- treefmt.nix
+++ treefmt.nix
@@ -10,13 +10,23 @@ let
     projectRootFile = "flake.nix";
     settings.verbose = 1;
     settings.excludes = [
-      "blobs/*"
+      ".gitignore"
       "secrets/*"
     ];
+    settings.on-unmatched = "debug";
 
-    programs.terraform.enable = true;
-    programs.nixfmt.enable = true;
-    programs.ruff-format.enable = true;
+    programs = {
+      terraform.enable = true;
+      nixfmt.enable = true;
+      jsonfmt.enable = true;
+      ruff-format.enable = true;
+      toml-sort.enable = true;
+      yamlfmt = {
+        enable = true;
+        excludes = [ "blobs/monitoring/oxidized/config.yml" ];
+        settings.formatter.retain_line_breaks = true;
+      };
+    };
   };
   package = forEachSystems (sys: treefmt-nix.lib.evalModule nixpkgs.legacyPackages.${sys} settings);
 in
