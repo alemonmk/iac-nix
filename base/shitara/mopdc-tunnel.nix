b6d2f83ca2ed2451e0be50e597c5988792b4b1b1
Lemon Lam <alemonmk@users.noreply.github.com>
2025-10-04 23:14:02 +0800

    feat(base/shitara/mopdc-tunnel.nix): clamp MSS to MTU

--- base/shitara/mopdc-tunnel.nix
+++ base/shitara/mopdc-tunnel.nix
@@ -112,6 +112,10 @@ in
     nat = {
       family = "ip";
       content = ''
+        chain clamp-mss {
+          type filter hook forward priority filter; policy accept
+          tcp flags syn tcp option maxseg size set rt mtu
+        }
         chain postrouting {
           type nat hook postrouting priority srcnat; policy accept
           oifname "eth0" ip saddr 10.0.0.0/8 ip daddr ne 10.0.0.0/8 counter masquerade
