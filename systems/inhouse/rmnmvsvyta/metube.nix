027bbafd825ad33605ce2eec6da91ab7bc904aa5
Lemon Lam <alemonmk@users.noreply.github.com>
2025-10-21 16:43:51 +0800

    update(systems/inhouse/rmnmvsvyta): metube 2025.09.27 -> 2025.10.15

--- systems/inhouse/rmnmvsvyta/metube.nix
+++ systems/inhouse/rmnmvsvyta/metube.nix
@@ -6,7 +6,7 @@
   };
 
   virtualisation.oci-containers.containers."metube" = {
-    image = "ghcr.io/alexta69/metube:2025.09.27";
+    image = "ghcr.io/alexta69/metube:2025.10.15";
     extraOptions = [ "--network=host" ];
     environment = {
       UID = "2500";
