f9a9843d6e1f49e139690e3b64ae9e29755d4604
Lemon Lam <alemonmk@users.noreply.github.com>
2025-12-08 11:38:01 +0800

    fix(systems/inhouse/rmnmvnfdns0{1,2}): drop web request proxy requirement

--- overlays/stable.nix
+++ overlays/stable.nix
@@ -26,10 +26,6 @@ final: prev: {
 
   squid = prev.squid.override { ipv6 = false; };
 
-  technitium-dns-server = prev.technitium-dns-server.overrideAttrs (old: rec {
-    patches = [ ../blobs/technitium-dns-server/webReqProxy.patch ];
-  });
-
   code-server = final.callPackage ../pkgs/code-server.nix { };
 
   vlmcsd = final.callPackage ../pkgs/vlmcsd.nix { };
