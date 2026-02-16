ed592636e0daed1afdfb704e9b40099ef9e93fa8
Lemon Lam <alemonmk@users.noreply.github.com>
2026-02-16 11:30:01 +0800

    chore(home/chisa/shells/nushell.nix): adjust config

--- home/chisa/shells/nushell.nix
+++ home/chisa/shells/nushell.nix
@@ -8,13 +8,11 @@ let
   resolvePath =
     s: lib.replaceStrings [ "$HOME" "$USER" ] [ config.home.homeDirectory config.home.username ] s;
   toNushellPathAdds = p: lib.concatMapStringsSep "\n" (s: "path add `" + (resolvePath s) + "`") p;
-  nuscripts = "${nixpkgs-next.nu_scripts}/share/nu_scripts";
 in
 {
   programs.nushell = {
     enable = true;
     package = nixpkgs-next.nushell;
-    environmentVariables = config.home.sessionVariables;
     shellAliases = {
       nopen = "open";
       open = "^open";
@@ -36,24 +34,28 @@ in
       };
     };
     extraConfig = ''
+      const NU_LIB_DIRS = [
+        "${nixpkgs-next.nu_scripts}/share/nu_scripts"
+      ]
       $env.SHELL = "nu"
 
       use std/util "path add"                       # Add paths using std path add (prepends by default)
       path add "/usr/local/bin"                     # Standard UNIX paths (add first = lower priority)
       path add "/nix/var/nix/profiles/default/bin"  # Nix paths (add last = higher priority)
       path add "/run/current-system/sw/bin"
-      path add ($env.HOME | path join ".nix-profile" "bin")
-      ${toNushellPathAdds config.home.sessionPath}
 
-      use ${nuscripts}/themes/nu-themes/catppuccin-latte.nu
+      use themes/nu-themes/catppuccin-latte.nu
       $env.config.color_config = (catppuccin-latte)
       $env.LS_COLORS = (${lib.getExe nixpkgs-next.vivid} generate catppuccin-latte)
 
       use std/dirs
       use std/dirs shells-aliases *
-
-      source ${nuscripts}/modules/filesystem/expand.nu
-      source ${nuscripts}/modules/network/sockets/sockets.nu
+    '';
+    extraLogin = ''
+      load-env ${lib.hm.nushell.toNushell { } config.home.sessionVariables}
+      use std/util "path add"
+      path add ($env.HOME | path join ".nix-profile" "bin")
+      ${toNushellPathAdds config.home.sessionPath}
     '';
   };
 }
