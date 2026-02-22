48126610f3e81d1a0d000cd73db4dc641244ed70
Lemon Lam <alemonmk@users.noreply.github.com>
2026-02-22 22:19:21 +0800

    refactor(lib/default.nix): boilerplates

--- lib/default.nix
+++ lib/default.nix
@@ -14,6 +14,35 @@ let
   linuxSystem = "x86_64-linux";
   darwinSystem = "x86_64-darwin";
   flakeRoot = ./..;
+
+  constructSystem = system: modules: {
+    specialArgs = {
+      inherit self flakeRoot;
+      nixpkgs-next = import nixpkgs-next {
+        inherit system;
+        overlays = [ (import ../overlays/next.nix) ];
+        config.allowUnfree = true;
+      };
+    };
+    inherit modules;
+  };
+  newLinuxSystem = modules: nixpkgs.lib.nixosSystem (constructSystem linuxSystem modules);
+  newDarwinSystem = modules: nix-darwin.lib.darwinSystem (constructSystem darwinSystem modules);
+
+  modulesInhouse = nixpkgs.lib.lists.flatten [
+    (nixpkgs.lib.attrsets.attrValues self.nixosModules)
+    impermanence.nixosModules.impermanence
+    sops-nix.nixosModules.sops
+    home-manager-linux.nixosModules.home-manager
+    ../base/inhouse/configuration.nix
+  ];
+  modulesLinodes = [
+    self.nixosModules.vpn-route-gen
+    sops-nix.nixosModules.sops
+    home-manager-linux.nixosModules.home-manager
+    ../base/linode/configuration.nix
+  ];
+  modulesDarwin = [ home-manager-darwin.darwinModules.home-manager ];
 in
 {
   stage1System = nixpkgs.lib.nixosSystem {
@@ -26,50 +55,8 @@ in
   stage1LinodeSystem = nixpkgs.lib.nixosSystem {
     modules = [ ../barebone/linode/configuration.nix ];
   };
-  finalSystem =
-    sysDef:
-    nixpkgs.lib.nixosSystem {
-      specialArgs = {
-        inherit flakeRoot;
-        nixpkgs-next = import nixpkgs-next {
-          system = linuxSystem;
-          overlays = [ (import ../overlays/next.nix) ];
-          config.allowUnfree = true;
-        };
-      };
-      modules =
-        nixpkgs.lib.attrsets.attrValues self.nixosModules
-        ++ [
-          impermanence.nixosModules.impermanence
-          sops-nix.nixosModules.sops
-          home-manager-linux.nixosModules.home-manager
-          ../base/inhouse/configuration.nix
-        ]
-        ++ sysDef;
-    };
-  finalLinodeSystem =
-    sysDef:
-    nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit flakeRoot; };
-      modules = [
-        self.nixosModules.vpn-route-gen
-        sops-nix.nixosModules.sops
-        home-manager-linux.nixosModules.home-manager
-        ../base/linode/configuration.nix
-      ]
-      ++ sysDef;
-    };
-  finalDarwinSystem =
-    sysDef:
-    nix-darwin.lib.darwinSystem {
-      specialArgs = {
-        inherit flakeRoot;
-        nixpkgs-next = import nixpkgs-next {
-          system = darwinSystem;
-          config.allowUnfree = true;
-        };
-      };
-      modules = [ home-manager-darwin.darwinModules.home-manager ] ++ sysDef;
-    };
+  finalSystem = sysDef: newLinuxSystem (modulesInhouse ++ sysDef);
+  finalLinodeSystem = sysDef: newLinuxSystem (modulesLinodes ++ sysDef);
+  finalDarwinSystem = sysDef: newDarwinSystem (modulesDarwin ++ sysDef);
   linuxPackageFrom = f: nixpkgs.legacyPackages."${linuxSystem}".callPackage f { };
 }
