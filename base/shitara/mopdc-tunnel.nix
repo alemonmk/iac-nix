97b74ae16b270c18e9802fcd7502d1c0d9d8c5fc
Lemon Lam <alemonmk@users.noreply.github.com>
2026-03-13 01:16:04 +0800

    feat(base/shitara/mopdc-tunnel.nix): become effective on demand

--- base/shitara/mopdc-tunnel.nix
+++ base/shitara/mopdc-tunnel.nix
@@ -1,4 +1,5 @@
 {
+  flakeRoot,
   config,
   lib,
   pkgs,
@@ -7,115 +8,122 @@
 let
   inherit (lib.strings) optionalString concatLines;
   inherit (lib.lists) optional optionals;
-  inherit (config.networking) hostName;
-  netConfig = import ./netconfigs.nix { inherit hostName; };
-  wanAddress = netConfig.wan.v4;
-  localTunAddrV4 = netConfig.pdc-tunnel.local.v4;
-  localTunAddrV6 = optionalString (netConfig.pdc-tunnel.local ? v6) netConfig.pdc-tunnel.local.v6;
-  peerTunAddrV4 = netConfig.pdc-tunnel.peer.v4;
-  peerTunAddrV6 = optionalString (netConfig.pdc-tunnel.peer ? v6) netConfig.pdc-tunnel.peer.v6;
+  inherit (lib.attrsets) hasAttr;
+  inherit (lib.modules) mkIf mkAfter;
+  inherit (pkgs) replaceVarsWith;
+  netConfig = import ./netconfigs.nix { inherit (config.networking) hostName; };
 in
-{
-  systemd.network = {
-    config.networkConfig.IPv6Forwarding = localTunAddrV6 != "";
-    networks."2-eth0".xfrm = [ "xfrm0" ];
-    networks."10-xfrm0" = {
-      matchConfig.Name = "xfrm0";
-      linkConfig.MTUBytes = 1400;
-      address = [ "${localTunAddrV4}/31" ] ++ optional (localTunAddrV6 != "") "${localTunAddrV6}/64";
-    };
-    netdevs."10-xfrm0" = {
-      netdevConfig = {
-        Name = "xfrm0";
-        Kind = "xfrm";
+mkIf (netConfig ? pdc-tunnel) (
+  let
+    wanAddress = netConfig.wan.v4;
+    localTunAddrV4 = netConfig.pdc-tunnel.local.v4;
+    localTunAddrV6 = netConfig.pdc-tunnel.local.v6 or null;
+    peerTunAddrV4 = netConfig.pdc-tunnel.peer.v4;
+    peerTunAddrV6 = netConfig.pdc-tunnel.peer.v6 or null;
+  in
+  {
+    systemd.network = {
+      config.networkConfig.IPv6Forwarding = localTunAddrV6 != null;
+      networks."2-eth0".xfrm = [ "xfrm0" ];
+      networks."10-xfrm0" = {
+        matchConfig.Name = "xfrm0";
+        linkConfig.MTUBytes = 1400;
+        address = [ "${localTunAddrV4}/31" ] ++ optional (localTunAddrV6 != null) "${localTunAddrV6}/64";
       };
-      xfrmConfig.InterfaceId = netConfig.pdc-tunnel.xfrmId;
+      netdevs."10-xfrm0" = {
+        netdevConfig = {
+          Name = "xfrm0";
+          Kind = "xfrm";
+        };
+        xfrmConfig.InterfaceId = netConfig.pdc-tunnel.xfrmId;
+      };
     };
-  };
 
-  services.strongswan-swanctl = {
-    enable = true;
-    strongswan.extraConfig = ''
-      charon {
-        install_routes = no
-        install_virtual_ip = no
-        make_before_break = yes
-        send_vendor_id = yes
-      }
-    '';
-    swanctl.connections."to-mo-pdc" = {
-      version = 2;
-      local."0" = {
-        id = netConfig.pdc-tunnel.localId;
-        auth = "psk";
-      };
-      local_addrs = [ wanAddress ];
-      remote."0" = {
-        auth = "psk";
-        id = "@mo-n1.glb-tnsp.snct.rmntn.net";
-      };
-      remote_addrs = [ "%any" ];
-      proposals = [ "aes256gcm16-prfsha384-ecp384" ];
-      rekey_time = "8h";
-      dpd_delay = "10s";
-      if_id_in = builtins.toString netConfig.pdc-tunnel.xfrmId;
-      if_id_out = builtins.toString netConfig.pdc-tunnel.xfrmId;
-      children.default = {
-        esp_proposals = [ "aes256gcm16-ecp384" ];
-        local_ts = [ "0.0.0.0/0" ] ++ optional (localTunAddrV6 != "") "::/0";
-        remote_ts = [ "0.0.0.0/0" ] ++ optional (localTunAddrV6 != "") "::/0";
-      };
-    };
-    includes = [ config.sops.secrets.ipsec-psk.path ];
-  };
-
-  services.bird.config =
-    let
-      bgpV4Cfg = pkgs.replaceVarsWith {
-        src = flakeRoot + /blobs/shitara-overlay/bgp-peer-mopdc-v4.conf;
-        replacements = {
-          inherit localTunAddrV4 peerTunAddrV4;
+    services.strongswan-swanctl = {
+      enable = true;
+      strongswan.extraConfig = ''
+        charon {
+          install_routes = no
+          install_virtual_ip = no
+          make_before_break = yes
+          send_vendor_id = yes
+        }
+      '';
+      swanctl.connections."to-mo-pdc" = {
+        version = 2;
+        local."0" = {
+          id = netConfig.pdc-tunnel.localId;
+          auth = "psk";
+        };
+        local_addrs = [ wanAddress ];
+        remote."0" = {
+          auth = "psk";
+          id = "@mo-n1.glb-tnsp.snct.rmntn.net";
+        };
+        remote_addrs = [ "%any" ];
+        proposals = [ "aes256gcm16-prfsha384-ecp384" ];
+        rekey_time = "8h";
+        dpd_delay = "10s";
+        if_id_in = toString netConfig.pdc-tunnel.xfrmId;
+        if_id_out = toString netConfig.pdc-tunnel.xfrmId;
+        children.default = {
+          esp_proposals = [ "aes256gcm16-ecp384" ];
+          local_ts = [ "0.0.0.0/0" ] ++ optional (localTunAddrV6 != null) "::/0";
+          remote_ts = [ "0.0.0.0/0" ] ++ optional (localTunAddrV6 != null) "::/0";
         };
       };
-      bgpV6Cfg = pkgs.replaceVarsWith {
-        src = flakeRoot + /blobs/shitara-overlay/bgp-peer-mopdc-v6.conf;
-        replacements = {
-          inherit localTunAddrV6 peerTunAddrV6;
+      includes = [ config.sops.secrets.ipsec-psk.path ];
+    };
+
+    services.bird.config =
+      let
+        bgpV4Cfg = replaceVarsWith {
+          src = flakeRoot + /blobs/shitara-overlay/bgp-peer-mopdc-v4.conf;
+          replacements = {
+            inherit localTunAddrV4 peerTunAddrV4;
+          };
         };
-      };
-    in
-    lib.modules.mkAfter (
-      concatLines (
-        [ ''include "${bgpV4Cfg.outPath}";'' ]
-        ++ optional (localTunAddrV6 != "") ''include "${bgpV6Cfg.outPath}";''
-      )
-    );
+        bgpV6Cfg = replaceVarsWith {
+          src = flakeRoot + /blobs/shitara-overlay/bgp-peer-mopdc-v6.conf;
+          replacements = {
+            inherit localTunAddrV6 peerTunAddrV6;
+          };
+        };
+      in
+      mkAfter (
+        concatLines (
+          [ ''include "${bgpV4Cfg.outPath}";'' ]
+          ++ optional (localTunAddrV6 != null) ''include "${bgpV6Cfg.outPath}";''
+        )
+      );
 
-  services.vpn-route-gen.enable = true;
+    services.vpn-route-gen.enable = true;
 
-  networking.nftables.tables = {
-    global.content = ''
-      chain mopdc-input {
-        iifname "eth0" udp dport {isakmp, ipsec-nat-t} counter accept
-        iifname "xfrm0" ip saddr ${peerTunAddrV4} ip daddr ${localTunAddrV4} tcp dport bgp counter accept
-        iifname "xfrm0" ip saddr ${peerTunAddrV4} ip daddr ${localTunAddrV4} udp dport 3784 counter accept
-      }
-    ''
-    + optionalString (localTunAddrV6 != "") ''
-      chain mopdc-input {
-        iifname "xfrm0" ip6 saddr ${peerTunAddrV6} ip6 daddr ${localTunAddrV6} tcp dport bgp counter accept
-        iifname "xfrm0" ip6 saddr ${peerTunAddrV6} ip6 daddr ${localTunAddrV6} udp dport 3784 counter accept
-      }
-    ''
-    + ''
-      chain input {
-        jump mopdc-input
-      }
-    '';
-    nat.content = ''
-      chain source-nat {
-        oifname "eth0" ip saddr 10.0.0.0/8 ip daddr ne 10.0.0.0/8 counter masquerade
-      }
-    '';
-  };
-}
+    networking.nftables.tables = {
+      global.content = ''
+        chain mopdc-input {
+          iifname "eth0" udp dport {isakmp, ipsec-nat-t} counter accept
+          iifname "xfrm0" ip saddr ${peerTunAddrV4} ip daddr ${localTunAddrV4} tcp dport bgp counter accept
+          iifname "xfrm0" ip saddr ${peerTunAddrV4} ip daddr ${localTunAddrV4} udp dport 3784 counter accept
+        }
+      ''
+      + optionalString (localTunAddrV6 != null) ''
+        chain mopdc-input {
+          iifname "xfrm0" ip6 saddr ${peerTunAddrV6} ip6 daddr ${localTunAddrV6} tcp dport bgp counter accept
+          iifname "xfrm0" ip6 saddr ${peerTunAddrV6} ip6 daddr ${localTunAddrV6} udp dport 3784 counter accept
+        }
+      ''
+      + ''
+        chain input {
+          jump mopdc-input
+        }
+      '';
+
+      nat.content = ''
+        chain source-nat {
+          oifname "eth0" ip saddr 10.0.0.0/8 ip daddr ne 10.0.0.0/8 counter masquerade
+        }
+      '';
+    };
+  }
+)
--- systems/shitara/sena.nix
+++ systems/shitara/sena.nix
@@ -1,5 +1,4 @@
 { flakeRoot, ... }:
 {
   imports = [ (flakeRoot + /base/shitara/node.nix) ];
-  disabledModules = [ (flakeRoot + /base/shitara/mopdc-tunnel.nix) ];
 }
