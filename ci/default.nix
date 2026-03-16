14f9002bd1f6b8884fd917094e8711b1d9b1a1d2
Lemon Lam <alemonmk@users.noreply.github.com>
2026-03-16 22:59:43 +0800

    refactor(ci): reorganize

--- ci/default.nix
+++ ci/default.nix
@@ -1,18 +1,26 @@
-inputs:
+{
+  self,
+  nixpkgs,
+  nixpkgs-next,
+  ...
+}:
 let
-  inherit (inputs) self nixpkgs nixpkgs-next;
-  inherit (nixpkgs.lib) elem getName mapAttrs;
+  inherit (nixpkgs.lib.lists) elem;
+  inherit (nixpkgs.lib.strings) getName;
+  inherit (nixpkgs.lib.attrsets) mapAttrs;
+
   pkgs-stable = import nixpkgs {
-    system = "x86_64-linux";
+    localSystem = "x86_64-linux";
     config.allowUnfree = true;
     config.allowInsecurePredicate = pkg: elem (getName pkg) [ "squid" ];
   };
   pkgs-next = nixpkgs-next.legacyPackages.x86_64-linux;
-  stable-overlay-realised = (import ../overlays/stable.nix) pkgs-stable pkgs-stable;
-  next-overlay-realised = (import ../overlays/next.nix) pkgs-next pkgs-next;
+  stable-overlay-realised = (import ../overlays/stable.nix) { } pkgs-stable;
+  next-overlay-realised = (import ../overlays/next.nix) { } pkgs-next;
   overlays-combined = stable-overlay-realised // next-overlay-realised;
 in
 {
-  hosts.x86_64-linux = mapAttrs (_: cfg: cfg.config.system.build.toplevel) self.nixosConfigurations;
+  hosts.x86_64-linux =
+    self.nixosConfigurations |> mapAttrs (_: cfg: cfg.config.system.build.toplevel);
   pkgs.x86_64-linux = overlays-combined // self.packages.x86_64-linux;
 }
