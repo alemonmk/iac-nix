a8dd3f18ab1c8e725a9784a85fcaacddd46a0588
Lemon Lam <alemonmk@users.noreply.github.com>
2026-04-10 02:38:13 +0800

    feat(systems/shitara/kotone): Consul ACL project, secrets vault

--- systems/shitara/kotone.nix
+++ systems/shitara/kotone.nix
@@ -120,6 +120,35 @@
       '';
   };
 
+  services.vault = {
+    enable = true;
+    package = pkgs.vault-bin;
+    address = "10.85.183.6:8200";
+    listenerExtraConfig = ''
+      x_forwarded_for_authorized_addrs = "10.85.183.0/28,10.91.145.32/28"
+    '';
+    storageBackend = "raft";
+    storageConfig = ''
+      retry_join { leader_api_addr = "http://rmnmvsvisv01.snct.rmntn.net:8200" }
+      retry_join { leader_api_addr = "http://rmnmvsvisv02.snct.rmntn.net:8200" }
+      retry_join { leader_api_addr = "http://10.85.183.6:8200" }
+    '';
+    extraConfig = ''
+      ui = true
+      disable_mlock = true
+      api_addr = "http://10.85.183.6:8200"
+      cluster_name = "rmntn-secvault-1"
+      cluster_addr = "http://10.85.183.6:8201"
+      default_lease_ttl = "4h"
+      max_lease_ttl = "12h"
+      user_lockout "all" {
+        lockout_threshold = "3"
+        lockout_duration = "30m"
+        lockout_counter_reset = "15m"
+      }
+    '';
+  };
+
   networking.nftables.tables = {
     global.content =
       let
@@ -132,6 +161,8 @@
         chain service-input {
           iifname "ztinv*" ip daddr 10.85.183.6 tcp dport 6610 counter accept # OneDev
           iifname "ztinv*" ip daddr 10.85.183.6 tcp dport 25 counter accept # App mails
+          iifname ne "eth0" ip daddr 10.85.183.6 tcp dport 8200 counter accept # Vault API
+          iifname ne "eth0" ip saddr {10.85.101.9, 10.85.101.10} ip daddr 10.85.183.6 tcp dport 8201 counter accept # Vault cluster
           iifname "eth0" tcp dport 8472 counter accept # HatH
         }
       '';
