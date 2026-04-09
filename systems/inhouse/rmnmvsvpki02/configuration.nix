a217ce859b9b113ca699c3bc10bede103b6c198c
Lemon Lam <alemonmk@users.noreply.github.com>
2026-04-09 11:27:39 +0800

    chore(systems/inhouse/rmnmvsvpki02): merging PKI VLANs

--- systems/inhouse/rmnmvsvpki02/configuration.nix
+++ systems/inhouse/rmnmvsvpki02/configuration.nix
@@ -2,12 +2,12 @@
   systemd.network.networks."1-ens192" = {
     matchConfig.Name = "ens192";
     address = [
-      "10.85.101.17/28"
-      "2400:8902:e002:59ee::5701:ce01/64"
+      "10.85.101.2/29"
+      "2400:8902:e002:59ef::5701:ce01/64"
     ];
     gateway = [
-      "10.85.101.30"
-      "2400:8902:e002:59ee::ccef"
+      "10.85.101.6"
+      "2400:8902:e002:59ef::ccef"
     ];
     networkConfig.LLDP = false;
   };
