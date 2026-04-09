f45e9ae9f8b489c2ab22a18dfe7c5c69dbdf7f0d
Lemon Lam <alemonmk@users.noreply.github.com>
2026-04-10 02:38:02 +0800

    feat(systems/inhouse/rmnmvsvisv02): Consul ACL project, secrets vault

---
+++ systems/inhouse/rmnmvsvisv02/configuration.nix
@@ -0,0 +1,18 @@
+{
+  systemd.network.networks."1-ens192" = {
+    matchConfig.Name = "ens192";
+    address = [
+      "10.85.101.10/29"
+      "2400:8902:e002:59ee::4c84:57a2/64"
+    ];
+    gateway = [
+      "10.85.101.14"
+      "2400:8902:e002:59ee::ccef"
+    ];
+    networkConfig.LLDP = false;
+  };
+
+  imports = [
+    ./vault.nix
+  ];
+}
---
+++ systems/inhouse/rmnmvsvisv02/vault.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  services = {
+    vault = {
+      enable = true;
+      package = pkgs.vault-bin;
+      address = "[::]:8200";
+      listenerExtraConfig = ''
+        x_forwarded_for_authorized_addrs = "10.85.183.0/28,10.91.145.32/28"
+      '';
+      storageBackend = "raft";
+      storageConfig = ''
+        retry_join { leader_api_addr = "http://rmnmvsvisv01.snct.rmntn.net:8200" }
+        retry_join { leader_api_addr = "http://rmnmvsvisv02.snct.rmntn.net:8200" }
+        retry_join { leader_api_addr = "http://10.85.183.6:8200" }
+      '';
+      extraConfig = ''
+        ui = true
+        disable_mlock = true
+        api_addr = "http://rmnmvsvisv02.scnt.rmntn.net:8200"
+        cluster_name = "rmntn-secvault-1"
+        cluster_addr = "http://rmnmvsvisv02.snct.rmntn.net:8201"
+        default_lease_ttl = "4h"
+        max_lease_ttl = "12h"
+        user_lockout "all" {
+          lockout_threshold = "3"
+          lockout_duration = "30m"
+          lockout_counter_reset = "15m"
+        }
+      '';
+    };
+  };
+
+  environment.persistence."/nix/persist".directories = [ "/var/lib/vault" ];
+}
