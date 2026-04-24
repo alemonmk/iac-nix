81883365d940863d7e252dbf50faa22c6cd47236
Lemon Lam <alemonmk@users.noreply.github.com>
2026-04-24 19:08:38 +0800

    feat(systems/inhouse/rmnmvsvisv01): Consul ACL project, deployment preparation

---
+++ systems/inhouse/rmnmvsvisv01/secrets.nix
@@ -0,0 +1,22 @@
+{
+  flakeRoot,
+  config,
+  pkgs,
+  ...
+}:
+{
+  sops.secrets = {
+    vault-mtls-key.sopsFile = flakeRoot + /secrets/secrets-vault/mtls-crt-key.yaml;
+    unseal-token-1.sopsFile = flakeRoot + /secrets/secrets-vault/unseal-tokens-a.yaml;
+    unseal-token-2.sopsFile = flakeRoot + /secrets/secrets-vault/unseal-tokens-a.yaml;
+  };
+  sops.templates."auto-unseal-conf" = {
+    file = pkgs.replaceVarsWith {
+      src = flakeRoot + /blobs/secrets-vault/auto-unseal.yaml;
+      replacements = {
+        unseal-token-1 = config.sops.placeholder.unseal-token-1;
+        unseal-token-2 = config.sops.placeholder.unseal-token-2;
+      };
+    };
+  };
+}
