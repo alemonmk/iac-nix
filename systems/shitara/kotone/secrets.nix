2e2a8c1173804798e5afe769832e152f224c36d9
Lemon Lam <alemonmk@users.noreply.github.com>
2026-04-24 19:09:12 +0800

    feat(systems/shitara/kotone): Consul ACL project, deployment preparation

--- systems/shitara/kotone/secrets.nix
+++ systems/shitara/kotone/secrets.nix
@@ -1,6 +1,7 @@
 {
   flakeRoot,
   config,
+  pkgs,
   ...
 }:
 {
@@ -9,6 +10,18 @@
     dkimkey = {
       owner = config.users.users.dkimsign.name;
       sopsFile = flakeRoot + /secrets/shitara/kotone/dkimkey.yaml;
+    };
+    vault-mtls-key.sopsFile = flakeRoot + /secrets/secrets-vault/mtls-crt-key.yaml;
+    unseal-token-1.sopsFile = flakeRoot + /secrets/secrets-vault/unseal-tokens-c.yaml;
+    unseal-token-2.sopsFile = flakeRoot + /secrets/secrets-vault/unseal-tokens-c.yaml;
+  };
+  sops.templates."auto-unseal-conf" = {
+    file = pkgs.replaceVarsWith {
+      src = flakeRoot + /blobs/secrets-vault/auto-unseal.yaml;
+      replacements = {
+        unseal-token-1 = config.sops.placeholder.unseal-token-1;
+        unseal-token-2 = config.sops.placeholder.unseal-token-2;
+      };
     };
   };
 }
