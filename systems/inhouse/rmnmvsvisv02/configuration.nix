ebdc14af5c8d59954292e6baf5135628fa93485e
Lemon Lam <alemonmk@users.noreply.github.com>
2026-04-24 19:08:54 +0800

    feat(systems/inhouse/rmnmvsvisv02): Consul ACL project, deployment preparation

--- systems/inhouse/rmnmvsvisv02/configuration.nix
+++ systems/inhouse/rmnmvsvisv02/configuration.nix
@@ -14,5 +14,6 @@
 
   imports = [
     ./vault.nix
+    ./secrets.nix
   ];
 }
--- systems/inhouse/rmnmvsvisv02/vault.nix
+++ systems/inhouse/rmnmvsvisv02/vault.nix
@@ -1,40 +1,64 @@
 {
+  flakeRoot,
   config,
-  lib,
   pkgs,
   ...
 }:
 {
-  services = {
-    vault = {
-      enable = true;
-      package = pkgs.vault-bin;
-      address = "[::]:8200";
-      listenerExtraConfig = ''
-        x_forwarded_for_authorized_addrs = "10.85.183.0/28,10.91.145.32/28"
-      '';
-      storageBackend = "raft";
-      storageConfig = ''
-        retry_join { leader_api_addr = "http://rmnmvsvisv01.snct.rmntn.net:8200" }
-        retry_join { leader_api_addr = "http://rmnmvsvisv02.snct.rmntn.net:8200" }
-        retry_join { leader_api_addr = "http://10.85.183.6:8200" }
-      '';
-      extraConfig = ''
-        ui = true
-        disable_mlock = true
-        api_addr = "http://rmnmvsvisv02.scnt.rmntn.net:8200"
-        cluster_name = "rmntn-secvault-1"
-        cluster_addr = "http://rmnmvsvisv02.snct.rmntn.net:8201"
-        default_lease_ttl = "4h"
-        max_lease_ttl = "12h"
-        user_lockout "all" {
-          lockout_threshold = "3"
-          lockout_duration = "30m"
-          lockout_counter_reset = "15m"
-        }
-      '';
+  services.openbao = {
+    enable = true;
+    package = pkgs.vault-bin;
+    settings = {
+      listener.default = {
+        type = "tcp";
+        address = "[::]:8200";
+        x_forwarded_for_authorized_addrs = "127.0.0.1,10.85.183.0/28,10.91.145.32/28";
+        tls_cert_file =
+          (pkgs.writeTextFile {
+            name = "vault-mtls-chain.crt";
+            text = ''
+              ${builtins.readFile (flakeRoot + /blobs/secrets-vault/mtls.crt)}
+              ${builtins.readFile (flakeRoot + /blobs/pki/v1.crt)}
+            '';
+          }).outPath;
+        tls_key_file = "/run/credentials/openbao.service/tls-key";
+        tls_min_version = "tls13";
+      };
+      api_addr = "https://secrets.snct.rmntn.net";
+      cluster_addr = "10.85.101.10:8201";
+      cluster_name = "rmntn-secrets-1";
+      disable_mlock = true;
+      max_lease_ttl = "12h";
+      default_lease_ttl = "4h";
+      user_lockout.all = {
+        lockout_threshold = "3";
+        lockout_duration = "30m";
+        lockout_counter_reset = "15m";
+      };
+      storage.raft = {
+        retry_join = [
+          { leader_api_addr = "https://10.85.101.9:8200"; }
+          { leader_api_addr = "https://10.85.101.10:8200"; }
+          { leader_api_addr = "https://10.85.183.6:8200"; }
+        ];
+        path = "/var/lib/openbao";
+      };
+      ui = true;
     };
   };
 
-  environment.persistence."/nix/persist".directories = [ "/var/lib/vault" ];
+  systemd.services.openbao.serviceConfig.LoadCredential =
+    "tls-key:${config.sops.secrets.vault-mtls-key.path}";
+
+  services.vault-unseal = {
+    enable = true;
+    configFile = config.sops.templates.auto-unseal-conf.path;
+  };
+
+  services.caddy = {
+    enable = true;
+    virtualHosts."secrets.snct.rmntn.net".extraConfig = "reverse_proxy https://localhost:8200";
+  };
+
+  environment.persistence."/nix/persist".directories = [ "/var/lib/private/openbao" ];
 }
