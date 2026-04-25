8e3ca7c671155675e6348d9a9fa23a73852d1fcd
Lemon Lam <alemonmk@users.noreply.github.com>
2026-04-25 19:03:09 +0800

    fix(pkgs/vault-unseal.nix): embed version information

--- pkgs/vault-unseal.nix
+++ pkgs/vault-unseal.nix
@@ -8,9 +8,16 @@ buildGo126Module (finalAttrs: {
   version = "1.0.0";
   src = fetchFromGitHub {
     owner = "lrstanley";
-    repo = finalAttrs.pname;
+    repo = "vault-unseal";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-czfG7DsA6O2n8BlzEEvNtu0Dg277qBnLAdVUZLo6+8w=";
+    hash = "sha256-ifCmojhePUwzgrMDen5tfLX6s9FyN1KmmuPJIb/WoxY=";
+    leaveDotGit = true;
+    postFetch = ''
+      cd "$out"
+      git rev-parse HEAD > $out/COMMIT
+      git log -1 --format=%cd --date=iso-strict > $out/SOURCE_DATE_EPOCH
+      find "$out" -name .git -exec rm -rf '{}' '+'
+    '';
   };
   vendorHash = "sha256-ma3xbnWH87b1X5fdOjigzsj5gEfhbjyTLoIDyp9eY80=";
 
@@ -18,7 +25,12 @@ buildGo126Module (finalAttrs: {
     "-s"
     "-w"
     "-extldflags=-static"
+    "-X main.version=${finalAttrs.version}"
   ];
+  preBuild = ''
+    ldflags+=" -X main.commit=$(cat COMMIT)"
+    ldflags+=" -X main.date=$(cat SOURCE_DATE_EPOCH)"
+  '';
   tags = [
     "netgo"
     "osusergo"
