f0262009409b9811c275195b866e5873e4a98890
Lemon Lam <alemonmk@users.noreply.github.com>
2026-04-26 22:12:50 +0800

    feat(systems/shitara/kotone/outbound-mailing.nix): allow vault-unseal to send notification mails

--- systems/shitara/kotone/outbound-mailing.nix
+++ systems/shitara/kotone/outbound-mailing.nix
@@ -33,18 +33,20 @@
       in
       ''
         table cluster-net { 10.85.183.0/28, 10.91.145.32/28 }
+        table secrets-vault { 10.85.101.9, 10.85.101.10 }
         table outbound-src { ${netConfig.wan.v4} }
         filter dkim-sign proc-exec "${dkimSignCmd}" user ${dkimSignUser} group ${dkimSignUser}
         listen on socket filter "dkim-sign"
         listen on ${netConfig.lo} port 25 filter "dkim-sign"
         action "outbound" relay src <outbound-src>
         match from src <cluster-net> for any action "outbound"
+        match from src <secrets-vault> for any action "outbound"
       '';
   };
 
   networking.nftables.tables.global.content = ''
     chain service-input {
-      iifname "ztinv*" ip daddr 10.85.183.6 tcp dport 25 counter accept
+      iifname ne "eth0" ip daddr 10.85.183.6 tcp dport 25 counter accept
     }
   '';
 }
