d4e04756709becdaa1524e2f775ce37f553e0ac1
Lemon Lam <alemonmk@users.noreply.github.com>
2026-05-26 21:49:34 +0800

    chore(lib): build package with nixpkgs-unstable

--- lib/default.nix
+++ lib/default.nix
@@ -81,8 +81,8 @@ let
   listFiles = base: base |> readDir |> filterAttrs (_: v: v == "regular") |> attrNames;
   listNixFiles = base: base |> listFiles |> filter (hasSuffix ".nix");
 
-  linuxPackageFrom = def: nixpkgs.legacyPackages."${linuxSystem}".callPackage def { };
-  linuxPackagesFrom = def: nixpkgs.legacyPackages."${linuxSystem}".callPackages def { };
+  linuxPackageFrom = def: nixpkgs-next.legacyPackages."${linuxSystem}".callPackage def { };
+  linuxPackagesFrom = def: nixpkgs-next.legacyPackages."${linuxSystem}".callPackages def { };
 in
 {
   inherit genAttrs;
