9d9b6178b4e4149ec38c9b40ab953e99d9be2e66
Lemon Lam <alemonmk@users.noreply.github.com>
2026-08-27 12:05:37 +0800

    chore(home/chisa/alemonmk/alacritty.nix): move colors to alacritty-theme

--- home/chisa/alemonmk/alacritty.nix
+++ home/chisa/alemonmk/alacritty.nix
@@ -1,6 +1,7 @@
 {
   programs.alacritty = {
     enable = true;
+    theme = "catppuccin_latte";
     settings = {
       general.live_config_reload = true;
       cursor = {
@@ -16,7 +17,6 @@
       window = {
         decorations = "Full";
         dynamic_title = true;
-        opacity = 0.8;
         option_as_alt = "Both";
         padding.x = 6;
         padding.y = 6;
@@ -26,80 +26,6 @@
         normal.family = "Input Mono";
         offset.x = 1;
         offset.y = 2;
-      };
-      colors = {
-        primary = {
-          background = "#eff1f5";
-          foreground = "#4c4f69";
-          dim_foreground = "#8c8fa1";
-          bright_foreground = "#4c4f69";
-        };
-        normal = {
-          black = "#bcc0cc";
-          red = "#d20f39";
-          green = "#40a02b";
-          yellow = "#df8e1d";
-          blue = "#1e66f5";
-          magenta = "#ea76cb";
-          cyan = "#179299";
-          white = "#5c5f77";
-        };
-        bright = {
-          black = "#acb0be";
-          red = "#d20f39";
-          green = "#40a02b";
-          yellow = "#df8e1d";
-          blue = "#1e66f5";
-          magenta = "#ea76cb";
-          cyan = "#179299";
-          white = "#6c6f85";
-        };
-        selection = {
-          text = "#eff1f5";
-          background = "#dc8a78";
-        };
-        cursor = {
-          text = "#eff1f5";
-          cursor = "#dc8a78";
-        };
-        vi_mode_cursor = {
-          text = "#eff1f5";
-          cursor = "#7287fd";
-        };
-        search = {
-          matches = {
-            foreground = "#eff1f5";
-            background = "#6c6f85";
-          };
-          focused_match = {
-            foreground = "#eff1f5";
-            background = "#40a02b";
-          };
-        };
-        footer_bar = {
-          foreground = "#eff1f5";
-          background = "#6c6f85";
-        };
-        hints = {
-          start = {
-            foreground = "#eff1f5";
-            background = "#df8e1d";
-          };
-          end = {
-            foreground = "#eff1f5";
-            background = "#6c6f85";
-          };
-        };
-        indexed_colors = [
-          {
-            index = 16;
-            color = "#fe640b";
-          }
-          {
-            index = 17;
-            color = "#dc8a78";
-          }
-        ];
       };
     };
   };
