3b8f2dae02b809588b32b9bd7e78c877e5cf80df
Lemon Lam <alemonmk@users.noreply.github.com>
2026-08-30 09:54:19 +0800

    update(systems/inhouse/rmnmvsvyta): metube 2026.05.30 -> 2026.08.28

--- systems/inhouse/rmnmvsvyta/metube.nix
+++ systems/inhouse/rmnmvsvyta/metube.nix
@@ -6,7 +6,7 @@
   };
 
   virtualisation.oci-containers.containers."metube" = {
-    image = "ghcr.io/alexta69/metube:2026.05.30";
+    image = "ghcr.io/alexta69/metube:2026.08.28";
     networks = [ "host" ];
     capabilities.all = false;
     user = "2500:2500";
