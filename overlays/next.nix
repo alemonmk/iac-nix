390270a42d3c9338307c2ffaa657bb56555373e7
Lemon Lam <alemonmk@users.noreply.github.com>
2026-09-24 18:48:12 +0800

    chore(overlays/next.nix): remove seaweedfs
    package is recent enough in unstable

--- overlays/next.nix
+++ overlays/next.nix
@@ -1,16 +1,1 @@
-final: prev: {
-  seaweedfs = prev.seaweedfs.overrideAttrs {
-    version = "4.29";
-    src = prev.pkgs.fetchFromGitHub {
-      owner = "seaweedfs";
-      repo = "seaweedfs";
-      tag = "4.29";
-      leaveDotGit = true;
-      postFetch = prev.seaweedfs.src.postFetch;
-      hash = "sha256-QvKcWIII31NNTKSINJC3wvQCzCHObBLO2c3r6cs2ges=";
-    };
-    vendorHash = "sha256-b4qgAdhLWL+J0WgKKHD8rxOd/IDI/xLF+tCXdAQrl1c=";
-    doCheck = false;
-    doInstallCheck = false;
-  };
-}
+final: prev: { }
